c2716d6ebdbf68115bc25084400024f488620015
gglennd <grdimaano18@gmail.com>
2025-08-05 09:35:17 +0800

    Added linter

---
+++ eslint.config.cjs
@@ -0,0 +1,25 @@
+const antfu = require("@antfu/eslint-config").default;
+const expoConfig = require("eslint-config-expo/flat");
+
+module.exports = antfu({
+  type: "app",
+  react: true,
+  typescript: true,
+  stylistic: {
+    indent: 2,
+    semi: true,
+    quotes: "double",
+  },
+  plugins: [expoConfig],
+  ignores: ["dist/**"],
+  rules: {
+    "no-console": ["warn"],
+    "perfectionist/sort-imports": ["error", {
+      tsconfigRootDir: ".",
+    }],
+    "unicorn/filename-case": ["error", {
+      case: "kebabCase",
+      ignore: ["README.md"],
+    }],
+  },
+});
